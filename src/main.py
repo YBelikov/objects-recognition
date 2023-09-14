a811d51b095fc1a1d26e9e4fbd28d494a7fc5686
Yuriy Belikov <>
2023-09-14 11:27:51 +0300

    Add click library to requirements + performed initial model test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
 def main():
-    car_detection_model = YOLO('models/yolov8n.pt')
-    license_plate_detection_model = YOLO('models/license_plate_recognition_model.pt')
-    license_plate_detection_model.predict()
-if __name__ == 'main':
+    car_detection_model = YOLO('../models/yolov8n.pt')
+    license_plate_detection_model = YOLO('../models/license_plate_recognition_model.pt')
+    license_plate_detection_model(source='../test-data/jj-jordan-jtZsFCvXS_g-unsplash.jpg', save=True, project='../results')
+
+if __name__ == '__main__':
     main()
